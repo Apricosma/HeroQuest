4d4149efbcccb49f7140192f40af89db688f4eb6
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-02-06 00:59:03 -0600

    Move branches

--- Program.cs
+++ Program.cs
@@ -1,17 +1,1 @@
-﻿namespace GameAssignment 
-{ 
-    class Program
-    {
-        static void Main(String[] args)
-        {
-            try
-            {
-                Game.StartGame();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-    }
-}
+﻿
